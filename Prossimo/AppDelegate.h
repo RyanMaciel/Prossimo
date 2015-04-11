0b9a7a996c29090d984286282086f16c459c27d4
Ryan Maciel <ryanmacieldevelopment@gmail.com>
2015-04-11 13:15:45 -0400

    Initial Commit

---
+++ Prossimo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prossimo
+//
+//  Created by Ryan Maciel on 4/11/15.
+//  Copyright (c) 2015 Prossimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
